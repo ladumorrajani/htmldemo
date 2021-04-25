3626e46a7354fcb6b32947c75516c957ffb1dcd0
Rajni1909 <Rajni.L@simformsolutions.com>
2021-04-25 19:31:26 +0530

    script add

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ import { GlobeKitView, Icosphere } from "./globekit.esm.js";
 const apiKey = 'gk_eb8cef449934d3357e1fe5b400483440a90febeb08bd29ff4195b47cecb62cd256401fbc6978f81f50a5b3a15a158588d569c8cc349f86d8fe37096c952b446c';
 const options = {
   apiKey: apiKey,
-  wasmPath: gkweb_bg.wasm,
+  wasmPath: './gkweb_bg.wasm',
 };
 
 this.gkview = new GlobeKitView(canvas, options);
