863061dd5a29568d57a9300ae656fac8ba48a28a
Rajni1909 <Rajni.L@simformsolutions.com>
2021-04-25 15:41:54 +0530

    Globe

--- README.md
+++
@@ -1,1 +0,0 @@
-# FounderByte Static HTML Site
---
+++ script.js
@@ -0,0 +1,15 @@
+import { GlobeKitView, Icosphere } from "globekit.esm.js";
+
+const apiKey = gk_eb8cef449934d3357e1fe5b400483440a90febeb08bd29ff4195b47cecb62cd256401fbc6978f81f50a5b3a15a158588d569c8cc349f86d8fe37096c952b446c;
+const options = {
+  apiKey: apiKey,
+  wasmPath: gkweb_bg.wasm,
+};
+
+this.gkview = new GlobeKitView(canvas, options);
+
+this.icosphere = new Icosphere('disk-3.png');
+
+this.gkview.addDrawable(this.icosphere, () => {
+  this.gkview.startDrawing();
+});
